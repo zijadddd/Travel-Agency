f538057d58a831ed918a24723c67a39f95004530
zijadddd <zijad.doglod.20@size.ba>
2023-03-26 16:48:21 +0200

    Out model added

---
+++ Travel Agency/Models/Out/UserOut.cs
@@ -0,0 +1,13 @@
+﻿namespace Travel_Agency.Models.Out {
+    public class UserOut {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public string Role { get; set; }
+    }
+}
